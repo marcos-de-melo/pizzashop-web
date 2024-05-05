9a22650ef9fa603d6cf6448af40efb8f47f6b7da
Marcos de Melo <marcdmelo@gmail.com>
2024-05-05 13:35:24 -0300

    React Helmet (SEO)

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import './global.css'
 import { RouterProvider } from 'react-router-dom'
 
 import { router } from './routes'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 export function App() {
   return (
-      <RouterProvider router={router}/>
+      <HelmetProvider>
+        <Helmet titleTemplate="%s | pissa.shop"/>
+        <RouterProvider router={router}/>
+      </HelmetProvider>
   )
 }
