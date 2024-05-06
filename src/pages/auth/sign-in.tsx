ae10cd7df17b0f8b86a1ea8de26ed78773d48f64
Marcos de Melo <marcdmelo@gmail.com>
2024-05-06 16:46:12 -0300

    Notificações toast (sonner)

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,23 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { resolve } from "path";
 
 import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
+const signInForm = z.object({
+  email: z.string().email(),
+});
+type SignInForm = z.infer<typeof signInForm>;
 export function SignIn() {
+  const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignInForm>();
+
+  async function handleSignIn(data: SignInForm) {
+    console.log(data)
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  }
   return (
     <>
       <Helmet title="Login" />
@@ -15,16 +28,18 @@ export function SignIn() {
               Acessar painel
             </h1>
             <p className="text-sm text-muted-foreground">
-                Acompanhe suas vendas pelo painel do parceiro!
-                </p>
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
           </div>
-        <form className="space-y-4">
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
             <div className="space-y-2">
-                <Label htmlFor="email">Seu e-mail</Label>
-                <Input id="email" type="email"/>
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" {...register("email")} />
             </div>
-            <Button className="w-full" type="submit">Acessar painel</Button>
-        </form>
+            <Button disabled={isSubmitting} className="w-full" type="submit">
+              Acessar painel
+            </Button>
+          </form>
         </div>
       </div>
     </>
