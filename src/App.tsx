46cfe59373656a5c973cb938d944dd7448fd1bc5
Marcos de Melo <marcdmelo@gmail.com>
2024-05-07 10:31:35 -0300

    Página: Cadastro de restaurante

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import './global.css'
 
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 import { router } from './routes'
-import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 export function App() {
   return (
       <HelmetProvider>
         <Helmet titleTemplate="%s | pissa.shop"/>
+        <Toaster richColors />
         <RouterProvider router={router}/>
       </HelmetProvider>
   )
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,8 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { resolve } from "path";
-
+import { toast } from "sonner";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -16,7 +15,8 @@ export function SignIn() {
 
   async function handleSignIn(data: SignInForm) {
     console.log(data)
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+    toast.success('Enviamos um link de autenticação para seu e-mail.')
   }
   return (
     <>
