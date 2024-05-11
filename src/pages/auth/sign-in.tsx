9ef4d8f699457d6c66afa51df1c04a169ff35569
Marcos de Melo <marcdmelo@gmail.com>
2024-05-11 19:20:27 -0300

    Layout do app com cabeçalho

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { toast } from "sonner";
+import { Link } from 'react-router-dom'
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -14,14 +15,27 @@ export function SignIn() {
   const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignInForm>();
 
   async function handleSignIn(data: SignInForm) {
-    console.log(data)
-    await new Promise((resolve) => setTimeout(resolve, 2000))
-    toast.success('Enviamos um link de autenticação para seu e-mail.')
+    try{
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+      toast.success('Enviamos um link de autenticação para seu e-mail.',{
+          action: {
+              label: 'Reenviar',
+              onClick: ()=> handleSignIn(data),
+          },
+      })
+    }catch{
+      toast.error('Credenciais inválidas.')
+    }
   }
   return (
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant="ghost" asChild  className="absolute right-8 top-8">
+        <Link to="/sign-up">
+          Novo estabelecimento
+        </Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2x1 font-semibold tracking-tight">
