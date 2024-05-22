e6f9debd9023554d3deed212a4e9444d3541a2f3
Marcos de Melo <marcdmelo@gmail.com>
2024-05-22 12:41:04 -0300

    Gráfico de produtos populares

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -4,6 +4,7 @@ import { DayOrdersAmauntCard } from './day-orders-amaunt-card'
 import { MonthCanceledOrdersAmauntCard } from './month-canceled-orders-amount'
 import { MonthOrdersAmauntCard } from './month-orders-amount-card'
 import { MonthRevenueCard } from './month-revenue-card'
+import { RevenueChart } from './revenue-chart'
 
 export function Dashboard() {
   return (
@@ -17,6 +18,9 @@ export function Dashboard() {
           <MonthOrdersAmauntCard />
           <DayOrdersAmauntCard />
           <MonthCanceledOrdersAmauntCard />
+        </div>
+        <div className="grid grid-cols-9 gap-4">
+          <RevenueChart />
         </div>
       </div>
     </>
