39cf3a0542535474e883ce4a861ff2e0835ac608
Marcos de Melo <marcdmelo@gmail.com>
2024-06-28 16:13:01 -0300

    Loading dos gráficos

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import { resolve } from 'path'
 
 import { env } from '@/env'
 
@@ -10,7 +9,9 @@ export const api = axios.create({
 
 if (env.VITE_ENABLE_API_DELAY) {
   api.interceptors.request.use(async (config) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    await new Promise((resolve) =>
+      setTimeout(resolve, Math.round(Math.random() * 3000)),
+    )
     return config
   })
 }
