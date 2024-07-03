fc2393917b4c9e3d11bd6a615fa36524051f2688
Marcos de Melo <marcdmelo@gmail.com>
2024-07-03 14:49:09 -0300

    Utilizando Spies

--- src/components/order-status.spec.tsx
+++ src/components/order-status.spec.tsx
@@ -1,3 +1,18 @@
-test('1 + 1 equals 2', () => {
-  expect(1 + 1).toEqual(2)
+import { render } from '@testing-library/react'
+
+import { OrderStatus } from './order-status'
+
+describe('Order Status', () => {
+  it('should display the right text based on order status', () => {
+    const wrapper = render(<OrderStatus status="pending" />)
+
+    // wrapper.debug()
+    const statusText = wrapper.getByText('Pendente')
+    const badgeElement = wrapper.getByTestId('badge')
+
+    // console.log(statusText.outerHTML)
+    console.log(badgeElement.outerHTML)
+
+    expect(statusText).toBeInTheDocument()
+  })
 })
